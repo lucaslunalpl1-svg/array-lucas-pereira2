273ee720fe35aa11da83a9ae2170ee6c24058d73
lucaslunalpl1-svg <lucaslunalpl1@gmail.com>
2025-09-01 20:06:31 -0300

    Update array.py

--- array.py
+++ array.py
@@ -1,1 +1,7 @@
+colegas = ["Ana", "Bruno", "Carlos", "Daniela", "Eduardo"]
+print("Primeiro elemento:", colegas[0])
+print("Terceiro elemento:", colegas[2])
+print("Último elemento:", colegas[4])
+
+
 
